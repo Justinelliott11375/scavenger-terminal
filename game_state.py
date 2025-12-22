235fbdf9511a35a381ef84f43aab513cb2df1fde
Justin Elliott <Justinelliott11375@gmail.com>
2025-12-22 00:19:08 -0600

    bring Rabbit class into terminal session/runner workflow

--- game_state.py
+++ game_state.py
@@ -2,7 +2,13 @@ from transitions import Machine
 
 
 class GameState:
-	states = ['initial', 'keyboard_connected', 'treasure_island_book', 'chess_piece_book']
+	states = [
+		'initial',
+		'treasure_island_book',
+		'chess_piece_book',
+		'mirror_clue_directive_initial',
+		'mirror_clue_directive_repeat',
+	]
 
 	def __init__(self):
 		self.machine = Machine(model=self, states=GameState.states, initial='initial')
@@ -12,4 +18,14 @@ class GameState:
 		self.machine.add_transition(
 			trigger='discover_chess_piece_book', source='treasure_island_book', dest='chess_piece_book'
 		)
-		self.machine.add_transition(trigger='connect_keyboard', source='initial', dest='keyboard_connected')
+		self.machine.add_transition(trigger='connect_keyboard', source='initial', dest='mirror_clue_directive_initial')
+		self.machine.add_transition(
+			trigger='enter_mirror_clue_directive',
+			source='mirror_clue_directive_initial',
+			dest='mirror_clue_directive_initial',
+		)
+		self.machine.add_transition(
+			trigger='ran_mirror_clue_directive_initial',
+			source='mirror_clue_directive_initial',
+			dest='mirror_clue_directive_repeat',
+		)
